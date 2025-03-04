6fda559cca3049302776ecd92e33174494747976
SangaTenzin <tenzinsanga1@gmail.com>
2025-03-04 15:33:13 +0600

    Country

--- frappe/geo/doctype/country/country.py
+++ frappe/geo/doctype/country/country.py
@@ -13,10 +13,13 @@ class Country(Document):
 
 	if TYPE_CHECKING:
 		from frappe.types import DF
+		from hrms.hr.doctype.country_dsa_detail.country_dsa_detail import CountryDSADetail
 
 		code: DF.Data | None
 		country_name: DF.Data
+		currency: DF.Link | None
 		date_format: DF.Data | None
+		items: DF.Table[CountryDSADetail]
 		time_format: DF.Data | None
 		time_zones: DF.Text | None
 	# end: auto-generated types
